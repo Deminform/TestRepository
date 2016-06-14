ebc333254174072d1be189ba1ca9ddf80523a1c9
MASTER <deminform@gmail.com>
2016-06-14 17:02:15 +0300

    second push

--- source/java/task_01/Test.java
+++ source/java/task_01/Test.java
@@ -3,26 +3,24 @@ package task_01;
 public class Test {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            if (checkNumber(i) == true) {
+        for (int i = 2; i < 1000; i++) {
+            if (checkNumber(i)) {
                 System.out.print(i + ", ");
             }
         }
     }
 
     public static boolean checkNumber(int num){
-        int count = 0;
-        for (int i = 1; i <= num; i++) {
-            if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0) {
-                if (num > 1 && num / i == num || ((num % i == 0) && num / i == 1)) {
-                    count++;
+        for (int i = 2; i <= num; i++) {
+            for (int j = 2; j <= num; j++) {
+                if ((i * j) > num) {
+                    break;
+                }
+                if ((i * j) == num) {
+                    return false;
                 }
             }
         }
-
-        if (count == 2 || num == 2 || num == 3 || num == 5 || num == 7) {
-            return true;
-        }
-        else return false;
+        return true;
     }
 }
